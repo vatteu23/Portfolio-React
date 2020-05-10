54fdcad4072bba2a7c85e000d987ada1f3a226cd
Uday Vatti <vuday23@gmail.com>
2020-05-10 00:31:24 -0500

    Background Update

--- src/App.js
+++ src/App.js
@@ -68,7 +68,52 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles className="particles-bg" />
+        <Particles
+          className="particles-bg"
+          params={{
+            particles: {
+              number: {
+                value: 60,
+                density: {
+                  enable: true,
+                  value_area: 1500,
+                },
+              },
+              line_linked: {
+                enable: true,
+                opacity: 0.02,
+              },
+              move: {
+                direction: "right",
+                speed: 0.05,
+              },
+              size: {
+                value: 1,
+              },
+              opacity: {
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.05,
+                },
+              },
+            },
+            interactivity: {
+              events: {
+                onclick: {
+                  enable: true,
+                  mode: "push",
+                },
+              },
+              modes: {
+                push: {
+                  particles_nb: 1,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
         <ScrollToTop />
         <NavBar />
         <Switch>
